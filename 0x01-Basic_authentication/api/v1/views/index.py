0ca2fb3df0d5d2b5c4283c9c9061c3f2cded3d9a
Becky1703 <oluwabukunmia@gmail.com>
2023-11-07 10:58:50 +0000

    forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - the unauthorized error code
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - the forbidden error code
+    """
+    abort(403)
