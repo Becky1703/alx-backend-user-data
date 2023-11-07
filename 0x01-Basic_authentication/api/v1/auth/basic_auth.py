e5162aed3d38330b1c5cc23cdb876ce6705053fe
Becky1703 <oluwabukunmia@gmail.com>
2023-11-07 14:24:36 +0000

    user object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,8 @@ from api.v1.auth.auth import Auth
 import re
 import base64
 import binascii
-
+from typing import TypeVar
+from models.user import User
 
 class BasicAuth(Auth):
     """class inherits from Auth"""
@@ -48,3 +49,19 @@ class BasicAuth(Auth):
                 password = field_match.group('password')
                 return user, password
         return None, None
+
+    def user_object_from_credentials(
+            self,
+            user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Function retrieves a user based on the user's
+        authentication credentials"""
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+            return None
