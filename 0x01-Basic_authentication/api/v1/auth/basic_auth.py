0caf92cfab47b1467fbc390448d0a5193eaa0eb1
Becky1703 <oluwabukunmia@gmail.com>
2023-11-07 13:51:53 +0000

    base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,7 @@
 from api.v1.auth.auth import Auth
 import re
 import base64
-
+import binascii
 
 class BasicAuth(Auth):
     """class inherits from Auth"""
@@ -17,3 +17,17 @@ class BasicAuth(Auth):
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """Function returns the decoded value of a base64 string"""
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
