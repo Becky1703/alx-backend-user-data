fa4598055040d8a77204af49c51ec30b5f7c05d3
Becky1703 <oluwabukunmia@gmail.com>
2023-11-14 11:40:10 +0000

    find a user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,17 +41,16 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """Method finds a user based on set of filters"""
-        fields, values = [], []
-        for key, value in kwargs.items():
-            if hasattr(User, key):
-                fields.append(getattr(User, key))
-                values.append(value)
-            else:
-                raise InvalidRequestError()
-        result = self._session.query(User).filter(
-            tuple_(*fields).in_([tuple(values)])
-        ).first()
-        if result is None:
-            raise NoResultFound()
-        return result
-    
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+
+            if user is None:
+                raise NoResultFound
+
+            return user
+        except InvalidRequestError as e:
+            self._session.rollback()
+            raise e
+        except NoResultFound as e:
+            self._session.rollback()
+            raise e
