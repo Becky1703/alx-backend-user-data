889fda4ab9aea29f8939fd90c9f38d7cf4d197a6
Becky1703 <oluwabukunmia@gmail.com>
2023-11-14 12:14:23 +0000

    pycodestyle error fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,8 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 
+
 def _hash_password(password: str) -> bytes:
     """Method hashes a password"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-
- 
