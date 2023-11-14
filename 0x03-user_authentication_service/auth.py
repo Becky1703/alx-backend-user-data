6b2989c55a0b7e4b3ec382a7dcf88e9e8dd20e31
Becky1703 <oluwabukunmia@gmail.com>
2023-11-14 12:11:46 +0000

    hashed password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""User authentication"""
+import bcrypt
+
+
+from db import DB
+from user import User
+
+def _hash_password(password: str) -> bytes:
+    """Method hashes a password"""
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+ 
